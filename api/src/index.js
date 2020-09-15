f72057bd081cccb09db8e9d92d61a7e7e49176d0
Arthur <morinarthurpro@gmail.com>
2020-09-15 14:10:56 +0200

    first commit

---
+++ api/src/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express()
+const mysql = require('mysql2');
+const port = 8000;
+const domain = 'localhost';
+
+app.use(express.urlencoded({extended: false}))
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'arthur-db'
+  });
+
+
+connection.connect();
+
+
+app.post('/sign-up', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password
+    const userObject = {
+        name,
+        email,
+        password
+    }
+    connection.query("INSERT INTO user SET ?", userObject, function(err, result){
+        if(err) throw err;
+        console.log(`${userObject.name} has been added to the DB`);
+    });
+    res.send(name);
+})
+
+
+app.listen(port, function () {
+    console.log(`Listening on http://${domain}:${port}/`)
+ })
+
+
+app.post('/sign-in', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password
+})
